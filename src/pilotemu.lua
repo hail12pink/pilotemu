eace81a16739ee82fb32781dc13c4c338b0f7aaf
hail12pink <hail12pink@users.noreply.github.com>
2022-05-19 22:21:45 -0400

    added class compiler

--- src/pilotemu.lua
+++ src/pilotemu.lua
@@ -1,5 +1,27 @@
 local module = {}
 
+local ClassesFolder = script.Parent:WaitForChild("classes")
+local Classes = {}
+
+for _, module in pairs(classes:GetChildren()) do
+	if module:IsA("ModuleScript") then
+		local class, err = pcall(require, module)
+
+		if class then
+			table.insert(Classes, {
+				file = module,
+				class = class
+			})
+		elseif err then
+			error("An error occured while loading module '"..module.Name.."'")
+		else
+			error("An unknown error occured while loading module '"..module.Name.."'")
+		end
+	else
+		warn("Unexpected instance '" .. module.Name .. "' of class '" .. module.ClassName .. "' in ./classes/")
+	end
+end
+
 function module.new()
   return "hello world"
 end
